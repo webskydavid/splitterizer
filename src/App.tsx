c439e737ebf0dddc4f0d0af6882bbe5cf529107b
dawidsdh <dawid.schatton@start-up.house>
2022-07-18 18:43:25 +0200

    style: add some changes to styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,24 @@ import { DIRECTION } from './components/Group/constants/direction';
 
 const ITMES = 1500;
 const Divider = ({ title }: { title: string }) => {
-	return <div style={{ height: 40 }}>{title}</div>;
+	return <div style={{ height: 20 }}>{title}</div>;
 };
 
-const COMPONENTS1 = [
+const DIVIDERS_1 = [
 	null,
-	<Divider title="Test 1" />,
-	<Divider title="Test 1" />,
-	<Divider title="Test 1" />,
+	<Divider title="Divider 1" />,
+	<Divider title="Divider 2" />,
+	<Divider title="Divider 3" />,
 ];
 
-const COMPONENTS2 = [null, null, <Divider title="Test 1" />];
+const DIVIDERS_2 = [null, null, <Divider title="Divider 4" />];
 
 function App() {
 	const list = (length: number) => {
 		return Array(length)
 			.fill('Item')
 			.map((i, key) => (
-				<div key={key}>
+				<div key={key} style={{ color: 'gray' }}>
 					{i} {key}
 				</div>
 			));
@@ -31,7 +31,8 @@ function App() {
 		<div className={classes.root}>
 			<Group direction={DIRECTION.Row} scope="1" initSizes={[20, 60, 20]}>
 				<Box>
-					<Group direction={DIRECTION.Column} scope="2" components={COMPONENTS1}>
+					<Group direction={DIRECTION.Column} scope="2" components={DIVIDERS_1}>
+						<Box>{list(300)}</Box>
 						<Box>{list(300)}</Box>
 						<Box>{list(100)}</Box>
 						<Box>{list(50)}</Box>
@@ -42,12 +43,12 @@ function App() {
 					<Group
 						direction={DIRECTION.Column}
 						initSizes={[70, 10, 20]}
-						components={COMPONENTS2}
+						components={DIVIDERS_2}
 						scope="3"
 					>
-						<Box>fefe</Box>
-						<Box>dfe</Box>
-						<div>jfoewjfoe</div>
+						<Box>Something 1</Box>
+						<Box>Something 2</Box>
+						<div>Others</div>
 					</Group>
 				</Box>
 				<Box>fewf</Box>
